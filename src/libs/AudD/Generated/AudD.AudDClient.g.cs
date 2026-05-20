750c6ae6a1d1babf324c1e7adc649af9962218e3
Konstantin S. <havendv@gmail.com>
2026-05-20 18:55:55 +0400

    feat: Updated OpenAPI spec (#9)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/AudD/Generated/AudD.AudDClient.g.cs
+++ src/libs/AudD/Generated/AudD.AudDClient.g.cs
@@ -164,10 +164,7 @@ namespace AudD
         {
 
             HttpClient = httpClient ?? new global::System.Net.Http.HttpClient();
-            if (baseUri is not null)
-            {
-                HttpClient.BaseAddress ??= baseUri;
-            }
+            HttpClient.BaseAddress ??= baseUri ?? new global::System.Uri(DefaultBaseUrl);
             Authorizations = authorizations ?? new global::System.Collections.Generic.List<global::AudD.EndPointAuthorization>();
             Options = options ?? new global::AudD.AutoSDKClientOptions();
             _disposeHttpClient = disposeHttpClient;
@@ -280,7 +277,7 @@ namespace AudD
                 return explicitBaseUri;
             }
 
-            return ResolveSelectedServer()?.Uri ?? (s_availableServers.Length > 0 ? s_availableServers[0].Uri : HttpClient.BaseAddress);
+            return ResolveSelectedServer()?.Uri ?? HttpClient.BaseAddress;
         }
 
         private global::System.Uri? ResolveBaseUri(
